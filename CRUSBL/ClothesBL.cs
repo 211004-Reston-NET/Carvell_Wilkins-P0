9af69895322b5b2850c840193343d8f820258621
ccwilki1 <ccwilki1@gmail.com>
2021-10-19 20:16:33 -0400

    10.19 E changes

---
+++ CRUSBL/ClothesBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using RRModels;
+namespace CRUSBL
+{
+    public class ClothesBL
+    {
+        private Repository _repo;
+        public ClothesBL();
+        public List<Clothes> GetAllClothes()
+        {
+
+        }
+    }
+}
---
+++ CRUSDL/Repository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using CRUSMOD;
+using System.Collections.Generic;
+using System.Text.Json;
+     
+
+namespace CRUSDL
+{
+    public class Repository 
+    {
+        //folepath need to reference from the startup project (CRUS) and this is why
+        //we need to go back a folder and cd into CRUSDL
+       private const string _filepath = "./../CRUSDL/Database/CRUS.json";
+       private string _jsonstring; 
+
+       //public Clothes AddClothes(Clothes P_rest);
+        
+            //list<Clothes> listofClothes =
+
+        
+        public List<Clothes> GetAllClothes()
+            {
+             _jsonstring = File.ReadAllText(_filepath);
+
+             return JsonSerializer.Deserialize<List<Clothes>>(_jsonstring);
+            }
+
+
+    
+
+    }
+}
